82df79c56cd5bb3d908b83703448cd7556597575
Shyam820 <shyamgaderiya02@gmail.com>
2020-02-06 20:24:38 +0530

    Adding the project

---
+++ src/test/java/epam/com/epam_project/Newyeargift.java
@@ -0,0 +1,14 @@
+package epam.com.epam_project;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Newyeargift {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
